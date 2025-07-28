9803e49e7145a5967b2f6ee3530640ce1e7c75c8
burhankhaja <karhazemail@gmail.com>
2025-07-28 16:32:19 +0000

    Marketplace: separting error_codes from update_fee

--- marketplace/programs/marketplace/src/error.rs
+++ marketplace/programs/marketplace/src/error.rs
@@ -1,7 +1,10 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+pub enum MarketplaceErrors {
+    #[msg("Protocol fee can only be updated upto 0.5% of nft selling price")]
+    MaxFee,
+
+    #[msg("Admin can re-update fee after 7 week delay")]
+    FeeUpdateDelay,
 }
--- marketplace/programs/marketplace/src/instructions/update_fee.rs
+++ marketplace/programs/marketplace/src/instructions/update_fee.rs
@@ -1,5 +1,5 @@
-use crate::Global;
-use anchor_lang::{prelude::*, solana_program::clock};
+use crate::{Global, error::MarketplaceErrors};
+use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
 pub struct UpdateFee<'info> {
@@ -39,13 +39,4 @@ impl<'info> UpdateFee<'info> {
         );
         Ok(())
     }
-}
-
-#[error_code]
-pub enum MarketplaceErrors {
-    #[msg("Protocol fee can only be updated upto 0.5% of nft selling price")]
-    MaxFee,
-
-    #[msg("Admin can re-update fee after 7 week delay")]
-    FeeUpdateDelay,
 }
