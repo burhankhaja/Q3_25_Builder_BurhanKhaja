39eec5361e74a8e87d5725b9855d9c19b8a9854f
burhankhaja <karhazemail@gmail.com>
2025-08-17 05:25:02 +0000

    Capstone: adding new constant helpers: dailylamports and txfee

--- capstone/screen_wars/tests/constants.helpers.ts
+++ capstone/screen_wars/tests/constants.helpers.ts
@@ -4,3 +4,6 @@ export const ONE_HOUR_IN_SECONDS = 60 * 60;
 export const ONE_DAY_IN_SECONDS = 24 * ONE_HOUR_IN_SECONDS;
 export const TWENTY_ONE_DAY_IN_SECONDS = 21 * (24 * ONE_HOUR_IN_SECONDS);
 export const defaultPubkey = new PublicKey("11111111111111111111111111111111");
+
+export const DAILY_LAMPORTS = 10_000_000;
+export const txFeeInLamports = 5_000;
