2dc1cca1719d8006df56b1f055475b4c085f1b05
burhankhaja <karhazemail@gmail.com>
2025-08-17 05:28:09 +0000

    Capstone: adding another userflow helper to create challengeId 2 by berg and joined by apaar

--- capstone/screen_wars/tests/userflows.helpers.ts
+++ capstone/screen_wars/tests/userflows.helpers.ts
@@ -11,7 +11,7 @@ import * as clock from "./clock.helpers";
 import * as constants from "./constants.helpers";
 
 export async function adminInitializes__jeffCreatesChallenge__Shrinath_And_BergJoins(svm: LiteSVM, programId: PublicKey, admin: Keypair, jeff: Keypair, shrinath: Keypair, berg: Keypair) {
-    
+
     //// admin initializes
     let [globalPDA] = await state.getGlobalPDAAddressAndBump(programId);
     await methods.initialize(svm, programId, admin, globalPDA);
@@ -37,29 +37,31 @@ export async function adminInitializes__jeffCreatesChallenge__Shrinath_And_BergJ
 }
 
 
-export async function challengeRun___shrinathSlashedSomeDays__bergMaxStreak(svm: LiteSVM, programId: PublicKey,shrinath: Keypair, berg: Keypair, challengeId : number) {
+export async function challengeRun___shrinathSlashedSomeDays__bergMaxStreak(svm: LiteSVM, programId: PublicKey, shrinath: Keypair, berg: Keypair, challengeId: number) {
 
 
     //// @dev :: later :: use svm.setAccount on users pda to mock 19th days of perfect user state ... with both berg and shrinath having 18 streaks and locked_balance == 18 * 10mil lamports
-    
+
     //// timetravel to 19th day of challenge
-    let challengePDAData = await state.fetchChallengePda(svm, programId, challengeId, {consoleLog: false});
+    let challengePDAData = await state.fetchChallengePda(svm, programId, challengeId, { consoleLog: false });
     let challengeStartTime = challengePDAData.start;
 
 
-    let day19 = Number(challengeStartTime) + (constants.TWENTY_ONE_DAY_IN_SECONDS - (constants.ONE_DAY_IN_SECONDS * 2)); 
+    let day19 = Number(challengeStartTime) + (constants.TWENTY_ONE_DAY_IN_SECONDS - (constants.ONE_DAY_IN_SECONDS * 2));
 
     await clock.setTimeStamp(svm, day19);
 
     // berg passes on 19th day too
     await methods.syncAndLock(svm, programId, challengeId, berg, { logTxResult: false });
-    
+
     // shrinath fails on 19th day {if mocked prev state =>  slashed + streak_reset}
-    await methods.syncAndLock(svm, programId, challengeId, shrinath, { logTxResult: false, debug: {
-        user_passed: false,
-        days_not_synced: 0,
-        synced_today: false,
-    } });
+    await methods.syncAndLock(svm, programId, challengeId, shrinath, {
+        logTxResult: false, debug: {
+            user_passed: false,
+            days_not_synced: 0,
+            synced_today: false,
+        }
+    });
 
 
     //// time travel to 20th day , last day of challenge
@@ -68,11 +70,13 @@ export async function challengeRun___shrinathSlashedSomeDays__bergMaxStreak(svm:
 
 
     // berg just keeps passing 
-    await methods.syncAndLock(svm, programId, challengeId, berg, { logTxResult: false, debug: {
-        user_passed: true,
-        days_not_synced: 0,
-        synced_today: false,
-    } }); //@audit :: weird problem if used default syntax again here :: refer to BACKUP_TESS/IMPOSSIBLE_SYNCLOCK_ERROR.TS.md on local repo
+    await methods.syncAndLock(svm, programId, challengeId, berg, {
+        logTxResult: false, debug: {
+            user_passed: true,
+            days_not_synced: 0,
+            synced_today: false,
+        }
+    }); //@audit :: weird problem if used default syntax again here :: refer to BACKUP_TESS/IMPOSSIBLE_SYNCLOCK_ERROR.TS.md on local repo
 
     // shrinath passes on last day gets his positive streak back {streak: 1}
     await methods.syncAndLock(svm, programId, challengeId, shrinath, { logTxResult: false });
@@ -83,16 +87,14 @@ export async function challengeRun___shrinathSlashedSomeDays__bergMaxStreak(svm:
 }
 
 
-export async function teleportToChallengeEnd_____BergOutstreaksShrinath(svm: LiteSVM, programId: PublicKey,admin: Keypair, jeff: Keypair, shrinath: Keypair, berg: Keypair, challengeId : number) {
+export async function teleportToChallengeEnd_____BergOutstreaksShrinath(svm: LiteSVM, programId: PublicKey, admin: Keypair, jeff: Keypair, shrinath: Keypair, berg: Keypair, challengeId: number) {
 
     await adminInitializes__jeffCreatesChallenge__Shrinath_And_BergJoins(svm, programId, admin, jeff, shrinath, berg);
-    
+
     await challengeRun___shrinathSlashedSomeDays__bergMaxStreak(svm, programId, shrinath, berg, challengeId);
 }
 
-// add teleportToREwardClaiingPHase_________bergDeclaredWinner
-
-export async function teleportToRewardClaimingPhase_________bergDeclaredWinner(svm: LiteSVM, programId: PublicKey,admin: Keypair, jeff: Keypair, shrinath: Keypair, berg: Keypair, challengeId : number) {
+export async function teleportToRewardClaimingPhase_________bergDeclaredWinner(svm: LiteSVM, programId: PublicKey, admin: Keypair, jeff: Keypair, shrinath: Keypair, berg: Keypair, challengeId: number) {
 
     await teleportToChallengeEnd_____BergOutstreaksShrinath(svm, programId, admin, jeff, shrinath, berg, challengeId);
 
@@ -102,4 +104,34 @@ export async function teleportToRewardClaimingPhase_________bergDeclaredWinner(s
     // time travel to reward claiming phase
     let afterContentionPeriod = (await clock.now(svm) + (constants.ONE_DAY_IN_SECONDS * 5)); // since contention period spawn 5 days after end , and now is the 
     await clock.setTimeStamp(svm, afterContentionPeriod);
+}
+
+// creates another challenge (id: 2), (creator : berg) , (pariticipant : apaar; locked : 10_000_000) 
+export async function secondChallengeCreatedByBerg____JoinedAndLockedByApaar(svm: LiteSVM, programId: PublicKey, berg: Keypair, apaar: Keypair, challengeId: number) {
+
+    // berg creates challenge_id : 2
+    let startTime = await clock.now(svm) + constants.ONE_DAY_IN_SECONDS;
+    let dailyTimer = constants.ONE_HOUR_IN_SECONDS;
+    let [globalPDA] = await state.getGlobalPDAAddressAndBump(programId);
+    let globalPDADataBefore = await state.fetchGlobalPda(svm, programId);
+    const [challengePDA] = await state.getChallengePDAAddressAndBump(globalPDADataBefore.challenge_ids, programId);
+    await methods.createChallenge(svm, programId, startTime, dailyTimer, berg, globalPDA, challengePDA, { logTxResult: false });
+
+    // apaar joins challenge_id : 2
+    let [apaarUserPDA] = await state.getUserPDAAddressAndBump(apaar.publicKey, programId);
+    await methods.joinChallenge(svm, programId, globalPDADataBefore.challenge_ids, apaar, apaarUserPDA, globalPDA, challengePDA, { logTxResult: false });
+
+    // teleport to challenge start
+    let secondChallengeStart = Number((await state.fetchChallengePda(svm, programId, challengeId)).start);
+    await clock.setTimeStamp(svm, secondChallengeStart);
+
+    // apaar passes and syncs on first day
+    await methods.syncAndLock(svm, programId, challengeId, apaar, {
+        logTxResult: false,
+        debug: {
+            user_passed: true,
+            days_not_synced: 0,
+            synced_today: false
+        }
+    });
 }
